d3e246915c1b8cfaf500d15a688d24e7d202da98
Barry Knapp <bknapp@overstock.com>
2018-12-02 13:05:45 -0500

    lightbox

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Experience from './components/Experience';
 import Education from './components/Education';
 import Certificate from './components/Certificate';
 import Skills from './components/Skills';
-//import Lightbox from 'react-images';
+import LightboxGallery from './components/Gallery';
 
 
 class App extends Component {
@@ -18,9 +18,9 @@ class App extends Component {
 
 componentDidMount(){
   console.log('componentDidMount')
-//http://resumeapi-env.fp5zraupj7.us-east-2.elasticbeanstalk.com/resume/barry
+fetch('http://resumeapi-env.fp5zraupj7.us-east-2.elasticbeanstalk.com/resume/barry')
 //  fetch('http://resumeapi.knapp.work/resume/barry')
-  fetch('http://localhost:8080/resume/barry')
+//  fetch('http://localhost:8080/resume/barry')
   .then(results => {
     console.log(results)
       return results.json();
@@ -50,6 +50,7 @@ if(res.avatar==null){
         <div className='wrapper'>
           <div>
           <div className='sidebar'>
+
             <About
               avatar={res.avatar}
               name={res.name}
@@ -61,6 +62,11 @@ if(res.avatar==null){
 
           <div className='content-wrapper'>
             <div className='content'>
+              <LightboxGallery
+                images={[
+                  { src: '../media/knapp_work_arch.png'}
+                ]}
+              />
               <Experience experience={res.experience} />
               <Education education={res.education} />
               <Certificate certificate={res.certificate} />
